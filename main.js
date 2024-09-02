8124006b60149c24e91ee54d908672adfeb5ccf7
MatthewJRBradbury <48898619+MatthewJRBradbury@users.noreply.github.com>
2024-09-02 20:53:15 +1000

    navigation changes
    - burger menu updated from svg to html/css with animation css and onclick js
    - moved nav links into ul's

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-import './style.css';
+import "./style.css";
+
+// For click event and also reference for outer div tag
+const toggleMenu = document.querySelector("#mobile-nav-toggle");
+
+toggleMenu.onclick = () => {
+  toggleMenu.classList.toggle("hamburger-toggle");
+};
