9d11942a070ccef5598ab0b4d01f0d666fbf1646
MatthewJRBradbury <48898619+MatthewJRBradbury@users.noreply.github.com>
2024-09-04 01:05:50 +1000

    mobile menu + js toggle

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ import "./style.css";
 
 // For click event and also reference for outer div tag
 const toggleMenu = document.querySelector("#mobile-nav-toggle");
+const mobileMenuScreen = document.querySelector("#mobile-nav-menu");
+const body = document.body;
 
 toggleMenu.onclick = () => {
   toggleMenu.classList.toggle("hamburger-toggle");
+  mobileMenuScreen.classList.toggle("hidden");
+  body.classList.toggle("fixed");
 };
