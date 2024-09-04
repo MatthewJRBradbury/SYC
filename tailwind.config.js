f8241c6cddf965ab730799f55a45123e6aeded20
MatthewJRBradbury <48898619+MatthewJRBradbury@users.noreply.github.com>
2024-09-04 21:48:40 +1000

    fade animation for mobile menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,20 @@ export default {
         roseAccent: "#D4967D",
         roseAccentHover: "#D5A48E",
       },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        fadeOut: {
+          "0%": { opacity: "1" },
+          "100%": { opacity: "0" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn .25s ease-in",
+        fadeOut: "fadeOut .25s ease-out",
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
