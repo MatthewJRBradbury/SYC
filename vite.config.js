0109a71f18b63e105a3738dcf671334e746fbd03
MatthewJRBradbury <48898619+MatthewJRBradbury@users.noreply.github.com>
2024-09-25 14:31:31 +1000

    update links to have base URL of /SYC/ for Github pages (TEMP)
    temporary measure whilst in development

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
 export default defineConfig({
-  base: "./",
+  base: "./SYC/",
   css: {
     postcss: {
       plugins: [tailwindcss(), autoprefixer()],
