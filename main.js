7c04d7c4267c4b65d8f3653daadb86c1d72cc7b9
MatthewJRBradbury <48898619+MatthewJRBradbury@users.noreply.github.com>
2024-12-13 16:43:48 +1100

    image optimizations

--- main.js
+++ main.js
@@ -11,3 +11,30 @@ toggleMenu.onclick = () => {
   mobileMenuScreen.classList.toggle("hidden");
   body.classList.toggle("fixed");
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+  const highQualityImage = document.getElementById("high-quality-hero-img");
+
+  if (highQualityImage) {
+    // Check if the high-quality image has already loaded
+    if (!highQualityImage.complete) {
+      highQualityImage.classList.add("opacity-0");
+      highQualityImage.onload = function () {
+        // Hide placeholder after transition completes
+        highQualityImage.classList.remove("opacity-0");
+        setTimeout(() => {
+          highQualityImage.style.opacity = 1;
+        }, 300);
+        highQualityImage.style.opacity = 1;
+      };
+
+      highQualityImage.onerror = function () {
+        console.error("High-quality image failed to load");
+      };
+    } else {
+      // If the image is already loaded, show it immediately
+      highQualityImage.style.opacity = 1;
+      highQualityImage.classList.remove("opacity-0");
+    }
+  }
+});
